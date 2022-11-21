350b8322f2a22fa50b0e4c60b4f9938f28a8bd01
tomocafe <tomocafe@pm.me>
2022-11-20 16:50:02 -0800

    Enhance bb_now offset to support rounding up

--- src/util/time.sh
+++ src/util/time.sh
@@ -1,5 +1,9 @@
 # @package: util/time
 # Routines for common time and date operations
+# @example:
+# ```bash
+# # TODO
+# ```
 
 _bb_onfirstload "bb_util_time" || return
 
@@ -19,7 +23,7 @@ bb_load "util/math"
 # Returns a timestamp relative to the current time (in seconds after epoch)
 # @arguments:
 # - VAR: variable to store result (if not given, prints to stdout)
-# - OFFSET: {+,-}N{s,m,h,d,w} where N is an integer
+# - OFFSET: {+,-}N{s,m,h,d,w}[^] where N is an integer
 # @returns: 1 if any offset is invalid, 0 otherwise
 # @notes:
 #   s: seconds
@@ -27,6 +31,7 @@ bb_load "util/math"
 #   h: hours
 #   d: days
 #   w: weeks
+#   Optional: trailing ^ rounds up; ^d is short for +0d^
 function bb_now () {
     _bb_glopts "$@"; set -- "${__bb_args[@]}"
     local result
@@ -39,11 +44,17 @@ function bb_now () {
     local offset
     for offset in "$@"; do
         local isadd=true
+        local round=false
         case "${offset:0:1}" in
             +) ;;
             -) isadd=false ;;
+            ^) round=true ;;
             *) return "$__bb_false" ;;
         esac
+        if [[ "${offset: -1}" == "^" ]]; then
+            round=true
+            offset="${offset:0: -1}"
+        fi
         local unit=1
         case "${offset: -1}" in
             s) ;;
@@ -54,11 +65,26 @@ function bb_now () {
             *) return "$__bb_false" ;;
         esac
         local val="${offset:1: -1}"
+        [[ $val == "" ]] && val=0
         bb_isint "$val" || return "$__bb_false"
         if $isadd; then
             (( ts += val * unit ))
         else
             (( ts -= val * unit ))
+        fi
+        if $round; then
+            (( ts = ((ts / unit) + 1) * unit ))
+            if [[ $unit -gt 3600 ]]; then
+                # Need to consider timezone if rounding to start of day/week
+                local tz tzoff
+                printf -v tz '%(%z)T' # e.g., -0800, +0530
+                [[ $tz =~ [+-][01][0-9][03]0 ]] || return "$__bb_false"
+                (( tzoff = 10#${tz:3:2}*60 + 10#${tz:1:2}*3600 )) # specify base 10 due to leading zero
+                case "${tz:0:1}" in
+                    +) (( ts = ts - tzoff + 86400 )) ;;
+                    -) (( ts = ts + tzoff )) ;;
+                esac
+            fi
         fi
     done
     printf -v result '%(%s)T' "$ts"
