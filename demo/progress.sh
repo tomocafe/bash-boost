6e980bc2a45472a0fde5a84ae3d704a5799cff8f
tomocafe <tomocafe@pm.me>
2023-08-31 21:29:05 -0700

    Add progress demo

---
+++ demo/progress.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source "$BB_ROOT/bash-boost.sh"
+bb_load cli/progress
+
+for i in {0..100..5}; do
+    sleep 0.1s
+    bb_progressbar $i "Initializing"
+done
+echo
+
+for i in {0..100}; do
+    if [[ $i -gt 60 && $i -lt 70 ]]; then
+        sleep 0.5s
+        bb_progressbar $i "Thinking really hard"
+    else
+        sleep 0.1s
+        bb_progressbar $i "Reticulating splines"
+    fi
+done
+echo "Done."
+
